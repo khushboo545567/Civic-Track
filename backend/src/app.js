938eddd5f6fd9542d234e52c82d4e01520877b54
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-10-09 01:24:20 +0530

    removed app form the frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: "http://localhost:5173",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
