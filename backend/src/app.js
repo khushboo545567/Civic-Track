4cc1a644ca8ebabdcc3ec0ee6608fc9fbfebf177
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-10-09 12:04:51 +0530

    origin changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173https://civic-track-e6hx.onrender.com",
+    origin: "https://civic-track-e6hx.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
