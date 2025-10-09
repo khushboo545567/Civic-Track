b1a290539b4b828c2d197933d88c6eae0ad163ae
khushboo545567 <khushbooyadav6266763530@gmail.com>
2025-10-09 21:08:24 +0530

    .

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,10 @@
 import express from "express";
 import dbConnection from "./db/db.js";
 import { app } from "./app.js";
-
+const PORT = process.env.PORT || 3000;
 dbConnection()
   .then(() => {
-    app.listen(3000, () => {
+    app.listen(PORT, () => {
       console.log("Server started successfully");
     });
   })
